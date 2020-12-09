21ab92d3f1825c6c5289bf9b2f5ba0bf5a9dee98
Crowdbotics <team@crowdbotics.com>
2020-12-09 10:21:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/splashhomeapp_23272/MainActivity.java
@@ -0,0 +1,15 @@
+package com.splashhomeapp_23272;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "splashhomeapp_23272";
+    }
+}
